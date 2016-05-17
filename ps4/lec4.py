e5eaa194a2ceeaaeedc0445d3f17c8b904991073
Lily Xu <lilyxu@alum.mit.edu>
2016-05-17 19:21:21 -0400

    Add ps4 files

---
+++ ps4/lec4.py
@@ -0,0 +1,12 @@
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    while exp > 0:
+        power = base
+        power *= base
+        exp -= 1
+    print power
